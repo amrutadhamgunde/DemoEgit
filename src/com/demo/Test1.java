aa1913de37ec49133cc4d8c9400d46f4222be68e
amruta.dhamgunde <amruta.dhamgunde@INIDC-AMRUD.ad.diebold.com>
2021-03-11 17:51:46 +0530

    Initial commit - dev1

---
+++ src/com/demo/Test1.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Dev1 - Test class");
+
+	}
+
+}
